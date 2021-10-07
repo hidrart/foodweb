33960d19df25337e8de5ef1c09fbc332d8765543
hdytrfli <61191577+hidrart@users.noreply.github.com>
2021-10-07 12:42:35 +0700

    minus generate

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-document.addEventListener('DOMContentLoaded', function(event) {
-    updateFood();
-    addFood();
+document.addEventListener('DOMContentLoaded', async function(event) {
+    updateFood(await readData());
+    await addFood();
 });
 const readData = async () => {
     const url = 'http://127.0.0.1:3000/foods/';
@@ -10,7 +10,7 @@ const readData = async () => {
             "Content-Type": "application/json"
         },
     }).then(response => response.json()).then(data => data).catch(console.log);
-    return data
+    return await data
 }
 const getData = async (id) => {
     const url = 'http://127.0.0.1:3000/foods/'
@@ -20,7 +20,7 @@ const getData = async (id) => {
             "Content-Type": "application/json"
         },
     }).then(response => response.json()).then(data => data).catch(console.log);
-    return data
+    return await data
 }
 const putData = async (id, data) => {
     const url = 'http://127.0.0.1:3000/foods/';
@@ -51,7 +51,7 @@ const deleteData = async (id) => {
         },
     });
 }
-const createAlert = (message, alerttype) => {
+const createAlert = async (message, alerttype) => {
     var alert = document.createElement('div')
     var messageAlert = document.createElement('span')
     alert.setAttribute('id', 'alertInstance')
@@ -60,75 +60,74 @@ const createAlert = (message, alerttype) => {
     messageAlert.innerHTML = message;
     alert.appendChild(messageAlert)
     document.getElementById('myAlert').appendChild(alert)
-    setTimeout(function() {
-        document.getElementById("alertInstance").remove();
-    }, 3000);
+    await sleep(5000);
+    document.getElementById("alertInstance").remove();
 }
 const addFood = async () => {
     const form = document.getElementById('foodForm');
-    form.addEventListener('submit', (event) => {
+    form.addEventListener('submit', async (event) => {
         if (!form.checkValidity()) {
             event.preventDefault()
             event.stopPropagation()
             form.classList.add('was-validated');
-            createAlert("Data gagal di tambahkan", "alert-danger");
+            await createAlert("Data gagal di tambahkan", "alert-danger");
             setTimeout(function() {
                 form.classList.remove('was-validated');
-            }, 3000);
+            }, 5000);
         } else {
-            var data = readData();
             var food = {
-                id: data.length + 1,
+                id: await readData().length + 1,
                 nama: form.elements['nama'].value,
                 harga: form.elements['harga'].value,
-            };
-            createData(food);
-            updateFood();
-            createAlert("Data berhasil ditambahkan", "alert-success");
-            form.reset();
-            form.classList.remove('was-validated');
+            }
+            if (!await isContain(food)) {
+                createData(food);
+                await createAlert("Data berhasil ditambahkan", "alert-success");
+                form.reset();
+                form.classList.remove('was-validated');
+            } else {
+                await createAlert("Data sudah terdapat di Database", "alert-danger");
+            }
         }
-    }, false)
-}
-const deleteFood = async (id) => {
-    deleteData(id);
-    var data = await getData(id);
-    createAlert(`Data ${data.nama} berhasil di hapus`, "alert-warning");
+    });
+    updateFood(await readData());
 }
 const editFood = async (id) => {
     var data = await getData(id);
-    console.log(data);
     const form = document.getElementById('editFoodForm');
     form.querySelector('#nama').setAttribute('value', `${data.nama}`);
     form.querySelector('#harga').setAttribute('value', `${data.harga}`);
-    form.addEventListener('submit', (event) => {
+    form.addEventListener('submit', async (event) => {
         if (!form.checkValidity()) {
             event.preventDefault()
             event.stopPropagation()
             form.classList.add('was-validated');
-            createAlert("Data gagal di ubah", "alert-danger");
+            await createAlert("Data gagal di ubah", "alert-danger");
             setTimeout(function() {
                 form.classList.remove('was-validated');
-            }, 3000);
+            }, 5000);
         } else {
             var food = {
                 id: data.id,
                 nama: form.elements['nama'].value,
                 harga: form.elements['harga'].value,
             };
-            putData(data.id, food);
-            updateFood();
-            createAlert("Data berhasil diubah", "alert-success");
-            form.reset();
-            form.classList.remove('was-validated');
+            if (!await isContain(food)) {
+                putData(data.id, food);
+                await createAlert("Data berhasil diubah", "alert-success");
+                form.reset();
+                form.classList.remove('was-validated');
+            } else {
+                await createAlert("Data sudah terdapat di Database", "alert-danger");
+            }
         }
     }, false);
+    updateFood(await readData());
 }
-const updateFood = async () => {
-    var data = await readData();
+const updateFood = (data) => {
+    var counter = 1
     var table = document.getElementById('foodTable').querySelector('tbody');
     table.innerHTML = '';
-    var counter = 1
     data.forEach(food => {
         var row = table.insertRow(-1);
         row.insertCell(0).innerHTML = counter;
@@ -139,7 +138,7 @@ const updateFood = async () => {
         del.setAttribute('class', 'btn btn-sm btn-outline-danger ms-3');
         del.setAttribute('onClick', `deleteFood(${food.id})`);
         del.innerHTML = '<i class="far fa-trash-alt"></i>';
-        // create edit button
+        // create edit <button></button>
         var edit = document.createElement('button')
         edit.setAttribute('class', 'btn btn-sm btn-outline-dark ms-3')
         edit.setAttribute('data-bs-toggle', 'collapse')
@@ -156,3 +155,19 @@ const updateFood = async () => {
         counter++;
     })
 }
+const deleteFood = async (id) => {
+    await deleteData(id);
+    updateFood(await readData());
+    await createAlert(`Data ${await getData(id).nama} berhasil di hapus`, "alert-warning");
+}
+const isContain = async (food) => {
+    var data = await readData();
+    var flag = false;
+    data.forEach(item => {
+        if (item.nama == food.nama) {
+            flag = true;
+        }
+    });
+    return flag;
+}
+const sleep = (m) => new Promise(r => setTimeout(r, m))
