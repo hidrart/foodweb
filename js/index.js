665fbcd7937092ab2d01629e4c0cbabb110043ea
hdytrfli <61191577+hidrart@users.noreply.github.com>
2021-10-07 09:47:31 +0700

    CRUD complete

--- js/index.js
+++ js/index.js
@@ -1,10 +1,57 @@
-document.addEventListener('DOMContentLoaded', async function(event) {
-    const menuUrl = 'http://192.168.0.105:5500/menu.json';
-    const menu = await getMenu(menuUrl);
-    updateTable(menu)
-    addFood(menu);
+document.addEventListener('DOMContentLoaded', function(event) {
+    updateFood();
+    addFood();
 });
-const createAlert = async (message, alerttype) => {
+const readData = async () => {
+    const url = 'http://127.0.0.1:3000/foods/';
+    const data = await fetch(url, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        },
+    }).then(response => response.json()).then(data => data).catch(console.log);
+    return data
+}
+const getData = async (id) => {
+    const url = 'http://127.0.0.1:3000/foods/'
+    const data = await fetch(url + id, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        },
+    }).then(response => response.json()).then(data => data).catch(console.log);
+    return data
+}
+const putData = async (id, data) => {
+    const url = 'http://127.0.0.1:3000/foods/';
+    await fetch(url + id, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+}
+const createData = async (data) => {
+    const url = 'http://127.0.0.1:3000/foods/';
+    await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+}
+const deleteData = async (id) => {
+    const url = 'http://127.0.0.1:3000/foods/';
+    const response = await fetch(url + id, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        },
+    });
+}
+const createAlert = (message, alerttype) => {
     var alert = document.createElement('div')
     var messageAlert = document.createElement('span')
     alert.setAttribute('id', 'alertInstance')
@@ -17,56 +64,95 @@ const createAlert = async (message, alerttype) => {
         document.getElementById("alertInstance").remove();
     }, 3000);
 }
-const addFood = (menu) => {
-    'use-strict'
-    const form = document.querySelectorAll('.needs-validation');
-    Array.prototype.slice.call(form).forEach(function(form) {
-        form.addEventListener('submit', (event) => {
-            if (!form.checkValidity()) {
-                event.preventDefault()
-                event.stopPropagation()
-                form.classList.add('was-validated');
-                createAlert("Data gagal di tambahkan", "alert-danger");
-                setTimeout(function() {
-                    form.classList.remove('was-validated');
-                }, 3000);
-            } else {
-                let food = {
-                    id: menu.length + 1,
-                    name: form.elements['nama'].value,
-                    harga: form.elements['harga'].value,
-                };
-                console.log(food);
-                createAlert("Data berhasil di tambahkan", "alert-success");
-                form.reset();
+const addFood = async () => {
+    const form = document.getElementById('foodForm');
+    form.addEventListener('submit', (event) => {
+        if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+            form.classList.add('was-validated');
+            createAlert("Data gagal di tambahkan", "alert-danger");
+            setTimeout(function() {
                 form.classList.remove('was-validated');
-            }
-        }, false)
-    })
+            }, 3000);
+        } else {
+            var data = readData();
+            var food = {
+                id: data.length + 1,
+                nama: form.elements['nama'].value,
+                harga: form.elements['harga'].value,
+            };
+            createData(food);
+            updateFood();
+            createAlert("Data berhasil ditambahkan", "alert-success");
+            form.reset();
+            form.classList.remove('was-validated');
+        }
+    }, false)
 }
-const updateTable = (menu) => {
+const deleteFood = async (id) => {
+    deleteData(id);
+    var data = await getData(id);
+    createAlert(`Data ${data.nama} berhasil di hapus`, "alert-warning");
+}
+const editFood = async (id) => {
+    var data = await getData(id);
+    console.log(data);
+    const form = document.getElementById('editFoodForm');
+    form.querySelector('#nama').setAttribute('value', `${data.nama}`);
+    form.querySelector('#harga').setAttribute('value', `${data.harga}`);
+    form.addEventListener('submit', (event) => {
+        if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+            form.classList.add('was-validated');
+            createAlert("Data gagal di ubah", "alert-danger");
+            setTimeout(function() {
+                form.classList.remove('was-validated');
+            }, 3000);
+        } else {
+            var food = {
+                id: data.id,
+                nama: form.elements['nama'].value,
+                harga: form.elements['harga'].value,
+            };
+            putData(data.id, food);
+            updateFood();
+            createAlert("Data berhasil diubah", "alert-success");
+            form.reset();
+            form.classList.remove('was-validated');
+        }
+    }, false);
+}
+const updateFood = async () => {
+    var data = await readData();
     var table = document.getElementById('foodTable').querySelector('tbody');
-    menu.forEach(food => {
+    table.innerHTML = '';
+    var counter = 1
+    data.forEach(food => {
         var row = table.insertRow(-1);
-        var del = document.createElement('button')
-        del.setAttribute('class', 'btn btn-sm btn-danger me-3')
-        del.setAttribute('style', 'width: 8px, height: 8px')
-        del.innerHTML = '<i class="far fa-trash-alt"></i>';
-        var edit = document.createElement('button')
-        edit.setAttribute('class', 'btn btn-sm btn-success mr-3')
-        edit.setAttribute('style', 'width: 8px, height: 8px')
-        edit.innerHTML = '<i class="far fa-trash-alt"></i>';
-        row.insertCell(0).innerHTML = food.id;
+        row.insertCell(0).innerHTML = counter;
         row.insertCell(1).innerHTML = food.nama;
         row.insertCell(2).innerHTML = food.harga;
+        // create delete button
+        var del = document.createElement('button');
+        del.setAttribute('class', 'btn btn-sm btn-outline-danger ms-3');
+        del.setAttribute('onClick', `deleteFood(${food.id})`);
+        del.innerHTML = '<i class="far fa-trash-alt"></i>';
+        // create edit button
+        var edit = document.createElement('button')
+        edit.setAttribute('class', 'btn btn-sm btn-outline-dark ms-3')
+        edit.setAttribute('data-bs-toggle', 'collapse')
+        edit.setAttribute('data-bs-target', '#editFoodForm')
+        edit.setAttribute('aria-expanded', 'false')
+        edit.setAttribute('aria-controls', 'editFoodForm')
+        edit.setAttribute('onClick', `editFood(${food.id})`);
+        edit.innerHTML = '<i class="fas fa-edit"></i>';
+        // append button to action column
         action = row.insertCell(3)
         action.setAttribute('class', 'text-end')
         action.appendChild(del)
         action.appendChild(edit)
-    });
-}
-const getMenu = async (url) => {
-    const response = await fetch(url);
-    const data = await response.json().then(data => data.foods)
-    return data;
+        counter++;
+    })
 }
